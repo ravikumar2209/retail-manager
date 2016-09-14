fcaa8649faa6a2327177c5e10a6efcd6a5cc738a
Ravikumar V <ravvenkatara@paypal.com>
2016-09-14 20:33:02 +0530

    initial commit

---
+++ src/main/java/com/db/retailmanager/RetailManagerApplication.java
@@ -0,0 +1,12 @@
+package com.db.retailmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailManagerApplication.class, args);
+	}
+}
